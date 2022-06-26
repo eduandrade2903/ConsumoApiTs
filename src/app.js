ca43e9a01ac2ba06d05d9ffae3aa059c83763315
Gaabe-santos <gabriel.sts0@outlook.com>
2022-06-26 13:24:22 -0300

    Organizando o código

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 //importanto o express
 import express from "express";
 import db from "./config/dbConnect.js";
-import cadastros from "./models/Cadastro.js";
 import routes from "./routes/index.js";
 
 
@@ -13,32 +12,9 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(express.json());
-
-routes(app);
-
 // tranformar GET/PUT em objeto para conseguir manipular
 app.use(express.json());
 
-// GET by ID
-app.get('/api/v1/user/:id', (req, res) => {
-    let index = buscaCadastro(req.params.id);
-    res.json(cadastros[index]);
-});
-
-// PUT method
-app.put('/api/v1/user/:id', (req, res) => {
-    let index = buscaCadastro(req.params.id);
-    // esta alterando somente o name, o restante não
-    cadastros[index].name = req.body.name;
-    res.json(cadastros);
-});
-
-
-// Function to fetch ID
-function buscaCadastro(id) {
-    return cadastros.findIndex(cadastro => cadastro.id == id);
-}
-
+routes(app);
 
 export default app
